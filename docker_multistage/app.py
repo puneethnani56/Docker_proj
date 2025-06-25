2c052d13a0468e8cc9ebd07cf30db9d4d86192bb
puneethnani <puneethnani56@gmail.com>
2025-06-25 22:58:23 +0530

    Create app.py

---
+++ docker_multistage/app.py
@@ -0,0 +1,12 @@
+# app.py
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return f"Hello, {os.getenv('USER', 'World')}!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
